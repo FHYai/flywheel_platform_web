6fea08f02c7fe3fbcaf0167699d300a89e9865b2
冯海溢 <1966096797@qq.com>
2023-05-26 16:41:02 +0800

    更改系统名称

--- src/locales/lang/zh-CN/sys.ts
+++ src/locales/lang/zh-CN/sys.ts
@@ -67,8 +67,8 @@ export default {
     signUpFormTitle: '注册',
     forgetFormTitle: '重置密码',
 
-    signInTitle: '开箱即用的中后台管理系统',
-    signInDesc: '输入您的个人详细信息开始使用！',
+    signInTitle: '小冯专属后台管理系统',
+    signInDesc: '内心丰盈者，独行也如众！',
     policy: '我同意xxx隐私政策',
     scanSign: `扫码后点击"确认"，即可完成登录`,
 
